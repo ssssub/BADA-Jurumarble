252de9040b1c5488207692c1892e58c9d3718e50
ssssub <ssy46420902@gmail.com>
2026-02-17 14:29:41 +0900

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'db.xrmvmpbxrtldgpjtyklq.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // 필요한 경우 여기에 추가 설정을 넣습니다.
 };
 
 export default nextConfig;
